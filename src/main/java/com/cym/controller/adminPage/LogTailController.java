88381a65ce706ceec41b7fb2b8a90871c8bc31e6
cym1102 <cym1102@qq.com>
2021-10-11 13:50:22 +0800

    1

--- src/main/java/com/cym/controller/adminPage/LogTailController.java
+++ src/main/java/com/cym/controller/adminPage/LogTailController.java
@@ -16,9 +16,11 @@ import org.springframework.stereotype.Controller;
 
 import com.cym.model.Log;
 import com.cym.utils.ApplicationContextRegister;
+import com.cym.utils.SystemTool;
 import com.cym.utils.TailLogThread;
 
 import cn.craccd.sqlHelper.utils.SqlHelper;
+import cn.hutool.system.SystemUtil;
 
 @ServerEndpoint("/adminPage/logTail/{id}")
 @Controller
@@ -26,7 +28,6 @@ public class LogTailController {
 
 	private Process process;
 	private InputStream inputStream;
-
 
 	/**
 	 * 新的WebSocket请求开启
@@ -40,7 +41,11 @@ public class LogTailController {
 		try {
 			// 执行tail -f命令
 			Log log = sqlHelper.findById(id, Log.class);
-			process = Runtime.getRuntime().exec("tail -f " + log.getPath());
+			if (SystemTool.isWindows()) {
+				process = Runtime.getRuntime().exec("powershell Get-Content " + log.getPath() + " -Tail 20");
+			} else {
+				process = Runtime.getRuntime().exec("tail -f " + log.getPath() + " --line 20");
+			}
 			inputStream = process.getInputStream();
 
 			// 一定要启动新的线程，防止InputStream阻塞处理WebSocket的线程
