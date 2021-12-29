56a399ea3c24baee1d82b81dcb795c1c6e81388f
cym1102 <cym1102@qq.com>
2021-12-29 11:20:31 +0800

    1

--- src/test/java/com/cym/Doc.java
+++ src/test/java/com/cym/Doc.java
@@ -1,4 +1,5 @@
 package com.cym;
+
 import com.power.common.util.DateTimeUtil;
 import com.power.doc.constants.DocGlobalConstants;
 import com.power.doc.model.ApiConfig;
@@ -8,33 +9,31 @@ import org.noear.solonhat.smartdoc.SolonHtmlApiDocBuilder;
 
 public class Doc {
 
-    /**
-     * 包括设置请求头，缺失注释的字段批量在文档生成期使用定义好的注释
-     */
-    @Test
-    public void generate() {
-        ApiConfig config = new ApiConfig();
+	/**
+	 * 包括设置请求头，缺失注释的字段批量在文档生成期使用定义好的注释
+	 */
+	@Test
+	public void generate() {
+		ApiConfig config = new ApiConfig();
 
-        config.setServerUrl("http://localhost:8080");
+		config.setServerUrl("http://localhost:8080");
 
-        //设置用md5加密html文件名,不设置为true，html的文件名将直接为controller的名称
-        config.setMd5EncryptedHtmlName(true);
-        config.setStrict(false);//true会严格要求注释，推荐设置true
-        config.setOutPath(DocGlobalConstants.HTML_DOC_OUT_PATH);//输出到static/doc下
+		// 设置用md5加密html文件名,不设置为true，html的文件名将直接为controller的名称
+		config.setMd5EncryptedHtmlName(true);
+		config.setStrict(false);// true会严格要求注释，推荐设置true
+		config.setOutPath(DocGlobalConstants.HTML_DOC_OUT_PATH);// 输出到static/doc下
 
-        //不指定SourcePaths默认加载代码为项目src/main/java下的,如果项目的某一些实体来自外部代码可以一起加载
-        config.setSourceCodePaths(
-                SourceCodePath
-                        .path()
-                        .setDesc("smart-doc")
-                        .setPath("src/main/java/com/cym/controller/api/")
-        );
+		// 不指定SourcePaths默认加载代码为项目src/main/java下的,如果项目的某一些实体来自外部代码可以一起加载
+		config.setSourceCodePaths(//
+				SourceCodePath.path().setDesc("smart-doc").setPath("src/main/java/com/cym/controller/api/"), //
+				SourceCodePath.path().setDesc("smart-doc").setPath("src/main/java/com/cym/model/"), //
+				SourceCodePath.path().setDesc("smart-doc").setPath("src/main/java/com/cym/sqlhelper/"), //
+				SourceCodePath.path().setDesc("smart-doc").setPath("src/main/java/com/cym/utils/") //
+		);
 
-        long start = System.currentTimeMillis();
-//        ApiDocBuilder.buildApiDoc(config);
-        SolonHtmlApiDocBuilder.buildApiDoc(config);
-//        HtmlApiDocBuilder.builderControllersApi(config);//此处使用HtmlApiDocBuilder，ApiDocBuilder提供markdown能力
-        long end = System.currentTimeMillis();
-        DateTimeUtil.printRunTime(end, start);
-    }
+		long start = System.currentTimeMillis();
+		SolonHtmlApiDocBuilder.buildApiDoc(config);
+		long end = System.currentTimeMillis();
+		DateTimeUtil.printRunTime(end, start);
+	}
 }
