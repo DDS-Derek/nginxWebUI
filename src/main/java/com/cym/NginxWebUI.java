0144ba24e3d4978146b2682b3cda09c10ddf69ac
cym1102 <cym1102@qq.com>
2022-01-17 14:08:20 +0800

    1

--- src/main/java/com/cym/NginxWebUI.java
+++ src/main/java/com/cym/NginxWebUI.java
@@ -46,6 +46,10 @@ public class NginxWebUI {
 			app.onEvent(freemarker.template.Configuration.class, cfg -> {
 				cfg.setSetting("classic_compatible", "true");
 				cfg.setSetting("number_format", "0.##");
+				
+				cfg.setSetting("locale", "UTF-8");
+				cfg.setSetting("default_encoding", "UTF-8");
+				cfg.setSetting("url_escaping_charset", "UTF-8");
 			});
 
 		});
