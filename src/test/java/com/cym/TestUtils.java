36e93584bfb9e78adfb58544b85e70e85d2cb7bd
cym1102 <cym1102@qq.com>
2022-11-02 21:05:33 +0800

    1

--- src/test/java/com/cym/TestUtils.java
+++ src/test/java/com/cym/TestUtils.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import org.junit.Test;
 
+import cn.hutool.crypto.SecureUtil;
+
 public class TestUtils {
 
 	@Test
@@ -11,5 +13,11 @@ public class TestUtils {
 
 	}
 
+	public static void main(String[] args) {
+		String str = SecureUtil.md5("7974" + "%OHr1IIf6Gh19SXe2zI#y17ag$$%dw9p" + "18030546255" + "1667393791844");
 	
+		System.out.println(str);
+		
+		System.out.println(SecureUtil.md5(str));
+	}
 }
