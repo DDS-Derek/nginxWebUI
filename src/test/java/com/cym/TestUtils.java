695bffd656dacfa143a3430149b2891fb6dfeff2
cym1102 <cym1102@qq.com>
2022-11-23 15:08:12 +0800

    1

--- src/test/java/com/cym/TestUtils.java
+++ src/test/java/com/cym/TestUtils.java
@@ -16,28 +16,5 @@ public class TestUtils {
 
 	}
 
-	public static void main(String[] args) {
-		String username = "18030546255_1";
-		String uid = "7974";
-		String key = "wiqB2gaCMzP83Z8wh37L00uSWmgDcIvX";
 
-		String t = System.currentTimeMillis() + "";
-		String k = SecureUtil.md5(uid + key + username + t);
-		String sign = SecureUtil.md5(k);
-		String action = "login";
-
-		Map<String, Object> map = new HashMap<>();
-		map.put("username", username);
-		map.put("uid", uid);
-		map.put("t", t);
-		map.put("k", k);
-		map.put("sign", sign);
-		map.put("action", action);
-		
-		System.out.println(map);
-		
-		String rs = HttpUtil.get("https://www.freecdn.pw:8092/cdn/api/login.php", map);
-		
-		System.out.println(rs);
-	}
 }
