42fe388080f4e6da84dd7f7b0762022f8711263c
hfkgkjbkbn <12625328+hfkgkjbkbn@user.noreply.gitee.com>
2023-03-17 05:10:21 +0000

    对nginx启动命令进行校验，防止潜在的命令执行
    Signed-off-by: hfkgkjbkbn <12625328+hfkgkjbkbn@user.noreply.gitee.com>

--- src/main/java/com/cym/service/ConfService.java
+++ src/main/java/com/cym/service/ConfService.java
@@ -696,7 +696,6 @@ public class ConfService {
 		FileUtil.mkdir(confd);
 
 		// 写入主文件
-		nginxPath.replaceAll("\\.\\./","_");
 		FileUtil.writeString(nginxContent, nginxPath.replace(" ", "_"), StandardCharsets.UTF_8);
 		String decompose = settingService.get("decompose");
 
