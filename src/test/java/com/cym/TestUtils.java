a4a6f08101eb4e2b1f4e1d7f6c3dbf5577efa0cb
cym1102 <cym1102@qq.com>
2023-05-14 09:52:45 +0800

    1

--- src/test/java/com/cym/TestUtils.java
+++ src/test/java/com/cym/TestUtils.java
@@ -1,11 +1,29 @@
 package com.cym;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.http.HttpUtil;
+
 public class TestUtils {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
+
+		String pass = "abcde12345";
+		for (int i = 0; i < 100000; i++) {
+			pass += "abcde12345";
+		}
 		
-		
-		
+		FileUtil.writeString(pass, "d:\\1.txt", "UTF-8");
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "admin");
+		map.put("pass", pass);
+
+		String rs = HttpUtil.post("http://127.0.0.1:8080/adminPage/login/login", map);
+
+		System.err.println(rs);
 	}
 
 }
