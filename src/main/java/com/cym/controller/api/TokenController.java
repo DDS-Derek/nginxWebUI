207e7ea5c85e70472ce3db93e9458a6a22c4ee64
cym1102 <cym1102@qq.com>
2023-12-01 15:02:24 +0800

    升级smart-doc, 去除font.css内容

--- src/main/java/com/cym/controller/api/TokenController.java
+++ src/main/java/com/cym/controller/api/TokenController.java
@@ -24,6 +24,7 @@ public class TokenController extends BaseController {
 	/**
 	 * 获取Token
 	 * 
+	 * @apiNote 其他接口调用需要在http请求header中添加token, 其中token的获取需要先在管理员管理中, 打开用户的接口调用权限, 然后通过用户名密码调用获取token接口, 才能得到token    
 	 * @param name 用户名
 	 * @param pass 密码
 	 */
