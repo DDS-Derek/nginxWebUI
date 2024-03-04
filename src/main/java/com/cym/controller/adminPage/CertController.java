978378f7fc3757e92731e1914e529d477d29ecdc
cym1102 <cym1102@qq.com>
2024-03-04 13:49:17 +0800

    1

--- src/main/java/com/cym/controller/adminPage/CertController.java
+++ src/main/java/com/cym/controller/adminPage/CertController.java
@@ -291,7 +291,7 @@ public class CertController extends BaseController {
 
 	private String[] getEnv(Cert cert) {
 		List<String> list = new ArrayList<>();
-		list.add("HOME=" + homeConfig.home); // 指定acme证书存放目录 TODO 测试
+		list.add("HOME=" + homeConfig.home); // 指定acme证书存放目录
 		
 		if (cert.getDnsType().equals("ali")) {
 			list.add("Ali_Key=" + cert.getAliKey());
