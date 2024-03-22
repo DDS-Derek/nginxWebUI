3c8a5db50cd1f121c972303bcbeccac2f1edd0a6
cym1102 <cym1102@qq.com>
2024-03-22 20:25:04 +0800

    1

--- src/main/resources/static/js/adminPage/denyAllow/index.js
+++ src/main/resources/static/js/adminPage/denyAllow/index.js
@@ -7,7 +7,7 @@ $(function() {
 		}
 
 		form.render();
-	});	
+	});
 })
 
 
@@ -18,90 +18,94 @@ function search() {
 
 
 function add() {
-	$("#id").val(""); 
-	$("#name").val(""); 
-	$("#ip").val(""); 
+	$("#id").val("");
+	$("#name").val("");
+	$("#ip").val("");
 	showWindow(commonStr.add);
 }
 
 
-function showWindow(title){
+function showWindow(title) {
 	layer.open({
-		type : 1,
-		title : title,
-		area : [ '1000px', '610px' ], // 宽高
-		content : $('#windowDiv')
+		type: 1,
+		title: title,
+		area: ['1000px', '610px'], // 宽高
+		content: $('#windowDiv')
 	});
 }
 
 function addOver() {
-	
+	if ($("#name").val() == '' || $("#ip").val() == '') {
+		layer.msg(serverStr.noFill);
+		return;
+	}
+
 	$.ajax({
-		type : 'POST',
-		url : ctx + '/adminPage/denyAllow/addOver',
-		data : $('#addForm').serialize(),
-		dataType : 'json',
-		success : function(data) {
+		type: 'POST',
+		url: ctx + '/adminPage/denyAllow/addOver',
+		data: $('#addForm').serialize(),
+		dataType: 'json',
+		success: function(data) {
 			if (data.success) {
 				location.reload();
 			} else {
 				layer.msg(data.msg);
 			}
 		},
-		error : function() {
+		error: function() {
 			layer.alert(commonStr.errorInfo);
 		}
 	});
-	
+
 }
 
 
 function edit(id) {
-	$("#id").val(id); 
-	
+	$("#id").val(id);
+
 	$.ajax({
-		type : 'GET',
-		url : ctx + '/adminPage/denyAllow/detail',
-		dataType : 'json',
-		data : {
-			id : id
+		type: 'GET',
+		url: ctx + '/adminPage/denyAllow/detail',
+		dataType: 'json',
+		data: {
+			id: id
 		},
-		success : function(data) {
+		success: function(data) {
 			if (data.success) {
 				var denyAllow = data.obj;
-				$("#id").val(denyAllow.id); 
-				$("#name").val(denyAllow.name); 
-				$("#ip").val(denyAllow.ip); 
-				
+				$("#id").val(denyAllow.id);
+				$("#name").val(denyAllow.name);
+				$("#ip").val(denyAllow.ip);
+
 				form.render();
 				showWindow(commonStr.edit);
-			}else{
+			} else {
 				layer.msg(data.msg);
 			}
 		},
-		error : function() {
+		error: function() {
 			layer.alert(commonStr.errorInfo);
 		}
 	});
 }
 
-function del(id){
-	if(confirm(commonStr.confirmDel)){
+function del(id) {
+	if (confirm(commonStr.confirmDel)) {
 		$.ajax({
-			type : 'POST',
-			url : ctx + '/adminPage/denyAllow/del',
-			data : {
-				id : id
+			type: 'POST',
+			url: ctx + '/adminPage/denyAllow/del',
+			data: {
+				id: id
 			},
-			dataType : 'json',
-			success : function(data) {
+			dataType: 'json',
+			success: function(data) {
 				if (data.success) {
 					location.reload();
-				}else{
+				} else {
 					layer.msg(data.msg)
 				}
 			},
-			error : function() {
+			error: function() {
 				layer.alert(commonStr.errorInfo);
 			}
 		});
