3326ec10e1682e5c64c536a75f9bb5719b258457
cym1102 <cym1102@qq.com>
2024-11-15 10:16:47 +0800

    1

--- src/test/java/com/cym/TestUtils.java
+++ src/test/java/com/cym/TestUtils.java
@@ -1,20 +1,19 @@
 package com.cym;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import org.noear.solon.test.HttpTester;
 import org.noear.solon.test.SolonTest;
 
 @SolonTest(NginxWebUI.class)
 public class TestUtils extends HttpTester {
+
 	@Test
-	@Ignore
 	public void test() {
 
 	}
 
 	public static void main(String[] args) throws Exception {
-		
+
 	}
 
 }
