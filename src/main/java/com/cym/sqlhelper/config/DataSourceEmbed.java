42d86c1aba0cf878cd79d27aa00e59626f211419
cym1102 <cym1102@qq.com>
2025-04-26 22:10:38 +0800

    1

--- src/main/java/com/cym/sqlhelper/config/DataSourceEmbed.java
+++ src/main/java/com/cym/sqlhelper/config/DataSourceEmbed.java
@@ -23,38 +23,41 @@ public class DataSourceEmbed {
 	@Inject("${spring.datasource.password}")
 	String password;
 
-	@Inject("${spring.database.tablePrefix:`}")
-	String TABLE_PREFIX = "`";
-	@Inject("${spring.database.tableSuffix:`}")
-	String TABLE_SUFFIX = "`";
-
-	@Inject("${spring.database.columnPrefix:`}")
-	String COLUMN_PREFIX = "`";
-	@Inject("${spring.database.columnSuffix:`}")
-	String COLUMN_SUFFIX = "`";
-
-	@Inject("${spring.database.idColumn:`id`}")
-	String ID_COLUMN = "`id`";
-
-	@Inject("${spring.database.orderTypeInt:SIGNED}")
-	String ORDER_TYPE_INT = "SIGNED";
-
-	@Inject("${spring.database.limitScript:(function() { return \" LIMIT \" + offset + \",\" + limit;})();}")
-	String LIMIT_SCRIPT = "(function() { return \" LIMIT \" + offset + \",\" + limit;})();";
+//	@Inject("${spring.database.tablePrefix:`}")
+//	String TABLE_PREFIX = "`";
+//	@Inject("${spring.database.tableSuffix:`}")
+//	String TABLE_SUFFIX = "`";
+//
+//	@Inject("${spring.database.columnPrefix:`}")
+//	String COLUMN_PREFIX = "`";
+//	@Inject("${spring.database.columnSuffix:`}")
+//	String COLUMN_SUFFIX = "`";
+//
+//	@Inject("${spring.database.idColumn:`id`}")
+//	String ID_COLUMN = "`id`";
+//
+//	@Inject("${spring.database.orderTypeInt:SIGNED}")
+//	String ORDER_TYPE_INT = "SIGNED";
+//
+//	@Inject("${spring.database.limitScript:(function() { return \" LIMIT \" + offset + \",\" + limit;})();}")
+//	String LIMIT_SCRIPT = "(function() { return \" LIMIT \" + offset + \",\" + limit;})();";
 
 	HikariDataSource dataSource;
 
 	@Init
 	public void init() {
-		SQLConstants.TABLE_SUFFIX=TABLE_SUFFIX;
-		SQLConstants.TABLE_PREFIX=TABLE_PREFIX;
-		SQLConstants.COLUMN_SUFFIX=COLUMN_SUFFIX;
-		SQLConstants.COLUMN_PREFIX=COLUMN_PREFIX;
-		SQLConstants.ID_COLUMN=ID_COLUMN;
-		SQLConstants.ORDER_TYPE_INT=ORDER_TYPE_INT;
-		SQLConstants.LIMIT_SCRIPT=LIMIT_SCRIPT;
+
 		// 创建dataSource
 		if (databaseType.equalsIgnoreCase("sqlite") || databaseType.equalsIgnoreCase("h2")) {
+
+			SQLConstants.TABLE_SUFFIX = "`";
+			SQLConstants.TABLE_PREFIX = "`";
+			SQLConstants.COLUMN_SUFFIX = "`";
+			SQLConstants.COLUMN_PREFIX = "`";
+			SQLConstants.ID_COLUMN = "`id`";
+			SQLConstants.ORDER_TYPE_INT = "SIGNED";
+			SQLConstants.LIMIT_SCRIPT = "(function() { return \" LIMIT \" + offset + \",\" + limit;})();";
+
 			// 建立新的sqlite数据源
 			HikariConfig dbConfig = new HikariConfig();
 			dbConfig.setJdbcUrl("jdbc:sqlite:" + homeConfig.home + "sqlite.db");
@@ -64,6 +67,15 @@ public class DataSourceEmbed {
 			dbConfig.setDriverClassName("org.sqlite.JDBC");
 			dataSource = new HikariDataSource(dbConfig);
 		} else if (databaseType.equalsIgnoreCase("mysql")) {
+
+			SQLConstants.TABLE_SUFFIX = "`";
+			SQLConstants.TABLE_PREFIX = "`";
+			SQLConstants.COLUMN_SUFFIX = "`";
+			SQLConstants.COLUMN_PREFIX = "`";
+			SQLConstants.ID_COLUMN = "`id`";
+			SQLConstants.ORDER_TYPE_INT = "SIGNED";
+			SQLConstants.LIMIT_SCRIPT = "(function() { return \" LIMIT \" + offset + \",\" + limit;})();";
+
 			HikariConfig dbConfig = new HikariConfig();
 			dbConfig.setJdbcUrl(url);
 			dbConfig.setUsername(username);
@@ -71,7 +83,16 @@ public class DataSourceEmbed {
 			dbConfig.setMaximumPoolSize(1);
 			dbConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
 			dataSource = new HikariDataSource(dbConfig);
-		}else {
+		} else if (databaseType.equalsIgnoreCase("postgresql")) {
+
+			SQLConstants.TABLE_SUFFIX = "\"";
+			SQLConstants.TABLE_PREFIX = "\"";
+			SQLConstants.COLUMN_SUFFIX = "\"";
+			SQLConstants.COLUMN_PREFIX = "\"";
+			SQLConstants.ID_COLUMN = "id";
+			SQLConstants.ORDER_TYPE_INT = "BIGINT";
+			SQLConstants.LIMIT_SCRIPT = "(function() { return \" LIMIT \" + limit + \" offset \" + offset; })();";
+
 			HikariConfig dbConfig = new HikariConfig();
 			dbConfig.setJdbcUrl(url);
 			dbConfig.setUsername(username);
