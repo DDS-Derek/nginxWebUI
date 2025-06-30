e7e07f593234a9ad54f5c7da9f94296d3442b41e
indsvr <2394857693@qq.com>
2025-06-30 17:39:52 +0800

    SSO-OAuth2.0

--- src/main/java/com/cym/controller/adminPage/SSOController.java
+++ src/main/java/com/cym/controller/adminPage/SSOController.java
@@ -1,6 +1,10 @@
 package com.cym.controller.adminPage;
 
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.cym.ext.AdminExt;
 import com.cym.ext.Tree;
 import com.cym.model.Admin;
@@ -9,16 +13,15 @@ import com.cym.service.AdminService;
 import com.cym.service.GroupService;
 import com.cym.service.SettingService;
 import com.cym.sqlhelper.bean.Page;
-import com.cym.utils.AuthUtils;
-import com.cym.utils.BaseController;
-import com.cym.utils.JsonResult;
-import com.cym.utils.SendMailUtils;
+import com.cym.sqlhelper.utils.ConditionAndWrapper;
+import com.cym.utils.*;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.jayway.jsonpath.JsonPath;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.annotation.Mapping;
@@ -49,148 +52,85 @@ public class SSOController extends BaseController {
 
 	@Mapping("")
 	public ModelAndView index(ModelAndView modelAndView) {
-		modelAndView.put("page", "");
+
+		modelAndView.put("codeUrl", settingService.get("sso_codeUrl"));
+		modelAndView.put("tokenUrl", settingService.get("sso_tokenUrl"));
+		modelAndView.put("userinfoUrl", settingService.get("sso_userinfoUrl"));
+		modelAndView.put("jsonpath", settingService.get("sso_jsonpath"));
+		modelAndView.put("clientID", settingService.get("sso_clientID"));
+		modelAndView.put("clientSecret", settingService.get("sso_clientSecret"));
 		modelAndView.view("/adminPage/sso/index.html");
 		return modelAndView;
 	}
 
-	@Mapping("addOver")
-	public JsonResult addOver(String id, String name, Boolean api, Integer type, String[] parentId) {
+	@Mapping("save")
+	public JsonResult save(String codeUrl, String tokenUrl, String userinfoUrl, String jsonpath, String clientID, String clientSecret,String callbackUrl) {
 
-		if (StrUtil.isEmpty(id)) {
-			Long count = adminService.getCountByName(name);
-			if (count > 0) {
-				return renderError(m.get("adminStr.nameRepetition"));
-			}
-		} else {
-			Long count = adminService.getCountByNameWithOutId(name, id);
-			if (count > 0) {
-				return renderError(m.get("adminStr.nameRepetition"));
-			}
-		}
-
-		Admin admin = new Admin();
-		admin.setId(id);
-		admin.setName(name);
-		admin.setApi(api);
-		admin.setType(type);
-
-		adminService.addOver(admin, parentId);
+		settingService.set("sso_codeUrl",codeUrl);
+		settingService.set("sso_tokenUrl",tokenUrl);
+		settingService.set("sso_userinfoUrl",userinfoUrl);
+		settingService.set("sso_jsonpath",jsonpath);
+		settingService.set("sso_clientID",clientID);
+		settingService.set("sso_clientSecret",clientSecret);
+		settingService.set("sso_callbackUrl",callbackUrl);
 
 		return renderSuccess();
 	}
 
-	@Mapping("changePassOver")
-	public JsonResult changePassOver(String id, String pass, Boolean auth) {
-		Admin admin = new Admin();
-		admin.setId(id);
-		admin.setPass(pass);
-		admin.setAuth(auth);
+	@Mapping("redirect")
+	public void redirect(Context ctx) {
 
-		adminService.changePassOver(admin);
 
-		return renderSuccess();
+		String codeUrl= settingService.get("sso_codeUrl");
+		String clientID=settingService.get("sso_clientID");
+		String callbackUrl=settingService.get("sso_callbackUrl");
+
+	  	String url = codeUrl+ "?client_id="+clientID+"&response_type=code&redirect_uri="+callbackUrl+"&oauth_timestamp="+System.currentTimeMillis()+"&state=";
+
+		ctx.redirect(url);
 	}
 
-	@Mapping("detail")
-	public JsonResult detail(String id) {
-		AdminExt adminExt = new AdminExt();
-		adminExt.setAdmin(sqlHelper.findById(id, Admin.class));
-		adminExt.setGroupIds(adminService.getGroupIds(adminExt.getAdmin().getId()));
-		adminExt.getAdmin().setPass("");
-		return renderSuccess(adminExt);
-	}
+	@Mapping("code")
+	public void code(String code,Context ctx) {
 
-	@Mapping("del")
-	public JsonResult del(String id) {
-		String[] ids = id.split(",");
-		sqlHelper.deleteByIds(ids, Admin.class);
+		String tokenUrl=settingService.get("sso_tokenUrl");
+		String userinfoUrl=settingService.get("sso_userinfoUrl");
+		String jsonpath=settingService.get("sso_jsonpath");
+		String clientID=settingService.get("sso_clientID");
+		String clientSecret=settingService.get("sso_clientSecret");
+		String callbackUrl=settingService.get("sso_callbackUrl");
 
-		return renderSuccess();
-	}
-	
-	
-	
 
-	@Mapping("getMailSetting")
-	public JsonResult getMailSetting() {
-		Map<String, String> map = new HashMap<>();
+		String getTokenUrl = tokenUrl + "?grant_type=authorization_code&oauth_timestamp="+System.currentTimeMillis()+"&client_id="+clientID+"&client_secret="+clientSecret
+				+"&code="+code+"&redirect_uri="+callbackUrl;
 
-		map.put("mail_host", settingService.get("mail_host"));
-		map.put("mail_port", settingService.get("mail_port"));
-		map.put("mail_from", settingService.get("mail_from"));
-		map.put("mail_user", settingService.get("mail_user"));
-		map.put("mail_pass", settingService.get("mail_pass"));
-		map.put("mail_ssl", settingService.get("mail_ssl"));
-		map.put("mail_interval", settingService.get("mail_interval"));
+		String post = HttpUtil.post(getTokenUrl, "");
 
-		return renderSuccess(map);
-	}
+		JSONObject entries = JSONUtil.parseObj(post);
+		String accessToken = entries.getStr("access_token");
 
-	@Mapping("updateMailSetting")
-	public JsonResult updateMailSetting(String mailType, String mail_user, String mail_host, String mail_port, String mail_from, String mail_pass, String mail_ssl, String mail_interval) {
-		settingService.set("mail_host", mail_host);
-		settingService.set("mail_port", mail_port);
-		settingService.set("mail_user", mail_user);
-		settingService.set("mail_from", mail_from);
-		settingService.set("mail_pass", mail_pass);
-		settingService.set("mail_ssl", mail_ssl);
-		settingService.set("mail_interval", mail_interval);
+		String userInfoUrl = userinfoUrl+"?access_token="+accessToken;
 
-		return renderSuccess();
-	}
+		String userinfoStr = HttpUtil.get(userInfoUrl);
 
-	@Mapping("testMail")
-	public JsonResult testMail(String mail) {
-		if (StrUtil.isEmpty(mail)) {
-			return renderError(m.get("adminStr.emailEmpty"));
-		}
-		try {
-			sendCloudUtils.sendMailSmtp(mail, m.get("adminStr.emailTest"), m.get("adminStr.emailTest"));
-			return renderSuccess();
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-			return renderError(m.get("commonStr.error") + ": " + e.getMessage());
-		}
-	}
+//
+//
+//		JSONObject jsonObject = JSONUtil.parseObj(userinfoStr);
 
-	@Mapping("testAuth")
-	public JsonResult testAuth(String key, String code) {
+		String read = JsonPath.read(userinfoStr, jsonpath);
 
-		Boolean rs = authUtils.testKey(key, code);
-		return renderSuccess(rs);
-	}
+		Admin admin = sqlHelper.findOneByQuery(new ConditionAndWrapper().eq(Admin::getName, read), Admin.class);
 
-	@Mapping(value = "qr")
-	public void getqcode(String url, Integer w, Integer h) throws IOException {
-		if (StrUtil.isNotBlank(url)) {
+		admin.setAutoKey(UUID.randomUUID().toString()); // 生成自动登录code
+		sqlHelper.updateById(admin);
 
-			if (w == null) {
-				w = 300;
-			}
-			if (h == null) {
-				h = 300;
-			}
-			try {
-				Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
-				hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
-				hints.put(EncodeHintType.MARGIN, 0);
+		Context.current().sessionSet("localType", "local");
+		Context.current().sessionSet("isLogin", true);
+		Context.current().sessionSet("admin", admin);
+		Context.current().sessionRemove("imgCode"); // 立刻销毁验证码
 
-				BitMatrix matrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, w, h, hints);
-				MatrixToImageWriter.writeToStream(matrix, "png", Context.current().outputStream());
-			} catch (WriterException e) {
-				logger.error(e.getMessage(), e);
-			}
-		}
-	}
 
-	@Mapping("getGroupTree")
-	public JsonResult getGroupTree() {
 
-		List<Group> groups = groupService.getListByParent(null);
-		List<Tree> treeList = new ArrayList<>();
-		remoteController.fillTree(groups, treeList);
-
-		return renderSuccess(treeList);
+		ctx.redirect("/adminPage/monitor");
 	}
 }
